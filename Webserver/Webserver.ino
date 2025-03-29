d7e371260726d4fa0cec56fed82b90f45741ec79
mathisdelbeke <mathisdelbeke.2003@hotmail.com>
2025-03-29 21:16:50 +0100

    New HTML page + refactored

--- Webserver/Webserver.ino
+++ Webserver/Webserver.ino
@@ -4,20 +4,24 @@
 // Replace with your network credentials
 const char* ssid = "x";
 const char* password = "x";
+const int ledPin = 2;
 
 WebServer server(80);  // Create a web server on port 80
 
-int ledPin = 2;  // LED connected to GPIO2 (onboard LED for most ESP32 boards)
-
 void setup() {
-  // Start the Serial monitor for debugging
   Serial.begin(115200);
   
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  init_wifi();
+  init_webserver();
+}
+
+void init_wifi() {
   Serial.print("Connecting to WiFi");
   
+  WiFi.begin(ssid, password);  // Connect to Wi-Fi
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
@@ -25,38 +29,43 @@ void setup() {
   
   Serial.println("Connected to WiFi!");
   Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());  // Print the IP address of the ESP32
-  
-  // Set LED pin as output
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);  // Start with the LED off
+  Serial.println(WiFi.localIP()); 
+}
 
-  // Define a simple HTTP route (this will serve a webpage)
+void init_webserver() {
   server.on("/", HTTP_GET, []() {
     String html = "<html><body>";
     html += "<h1>ESP32 Web Server</h1>";
-    html += "<p>LED is ";
+    html += "<p>LED is currently ";
     html += (digitalRead(ledPin) == HIGH) ? "ON" : "OFF";
     html += "</p>";
-    html += "<a href='/toggle'>Toggle LED</a>";
+    html += "<form method='get' action='/setled'>";
+    html += "<input type='radio' id='on' name='led' value='on' " + String(digitalRead(ledPin) == HIGH ? "checked" : "") + ">";
+    html += "<label for='on'>Turn LED On</label><br>";
+    html += "<input type='radio' id='off' name='led' value='off' " + String(digitalRead(ledPin) == LOW ? "checked" : "") + ">";
+    html += "<label for='off'>Turn LED Off</label><br>";
+    html += "<input type='submit' value='Submit'>";
+    html += "</form>";
     html += "</body></html>";
     server.send(200, "text/html", html);
   });
 
-  // Define a route to toggle the LED state
-  server.on("/toggle", HTTP_GET, []() {
-    int ledState = digitalRead(ledPin);  // Read current LED state
-    digitalWrite(ledPin, (ledState == LOW) ? HIGH : LOW);  // Toggle LED state
+  server.on("/setled", HTTP_GET, []() {
+    String ledState = server.arg("led");
     
-    // Redirect back to the main page
-    server.sendHeader("Location", "/");
-    server.send(303);  // HTTP status code for redirect
+    if (ledState == "on") {
+      digitalWrite(ledPin, HIGH); 
+    } else if (ledState == "off") {
+      digitalWrite(ledPin, LOW);  
+    }
+    
+    server.sendHeader("Location", "/");  // Redirect to main page
+    server.send(303);                    // HTTP status for redirect
   });
-
-  // Start the server
-  server.begin();
+  server.begin();  // Start the server
 }
 
 void loop() {
-  server.handleClient();  // Check if there are incoming client requests
+  // Check if there are incoming client requests
+  server.handleClient(); 
 }
