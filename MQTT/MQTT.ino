7c679172ef566ae5b8fa2f021fbbe08b2867f133
mathisdelbeke <mathisdelbeke.2003@hotmail.com>
2025-03-29 21:58:33 +0100

    MQTT publishes potentiometer value + refactored

--- MQTT/MQTT.ino
+++ MQTT/MQTT.ino
@@ -1,33 +1,43 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-// WiFi credentials
 const char* ssid = "x";
 const char* password = "x";
+const int potmeter_pin = 34;
 
-// MQTT broker (set PC IP if running Mosquitto locally)
-const char* mqtt_server = "192.168.0.154";  // Change this to your PC's IP
+// MQTT broker
+const char* mqtt_server = "192.168.0.154";
 const int mqtt_port = 1883;
-const char* mqtt_topic = "test";
+const char* mqtt_topic = "PotentioMeter";
+
+static unsigned long lastMsg = 0;
 
 // WiFi and MQTT client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup_wifi() {
-  delay(10);
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
+void setup() {
+  Serial.begin(115200);
 
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(on_topic_update);
+}
+
+void setup_wifi() {
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);  // Connect to Wi-Fi
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("\nWiFi connected. IP address: " + WiFi.localIP().toString());
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived [");
+void on_topic_update(char* topic, byte* message, unsigned int length) {
+  Serial.print("[");
   Serial.print(topic);
   Serial.print("]: ");
   for (int i = 0; i < length; i++) {
@@ -51,25 +61,15 @@ void reconnect() {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  setup_wifi();
-  
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-}
-
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
   
-  // Publish a message every 5 seconds
-  static unsigned long lastMsg = 0;
   if (millis() - lastMsg > 5000) {
     lastMsg = millis();
-    String msg = "Hello from ESP32!";
+    String msg = String(analogRead(potmeter_pin));
     client.publish(mqtt_topic, msg.c_str());
     Serial.println("Message sent: " + msg);
   }
